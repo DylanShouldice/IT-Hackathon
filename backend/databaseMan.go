66913f02b841040a6d8607d1ee4f852de189e113
Dylan Shouldice-Jacobs <Dshouldicejacob5799@conestogac.on.ca>
2025-03-07 23:47:22 -0500

    pushing stuff

--- backend/databaseMan.go
+++ backend/databaseMan.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"time"
 	"database/sql"
-    "fmt"
-    _ "github.com/go-sql-driver/mysql"
+	"fmt"
+	"strconv"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type NutritionValues struct {
@@ -18,15 +20,15 @@ type FoodEntry struct {
 	TimeStamp time.Time `json:"timestamp"`
 }
 
-type UserData struct{
-	ID int 
-	Password string
-	Height int
-	age int
+type UserData struct {
+	ID        int
+	Password  string
+	Height    int
+	age       int
 	allergies string
-	diet string
-	privacy bool
-	goal string
+	diet      string
+	privacy   bool
+	goal      string
 }
 
 func ConnectToDB() *sql.DB {
@@ -48,38 +50,36 @@ func ConnectToDB() *sql.DB {
 
 func writeUserData(user UserData, db *sql.DB) {
 	// write the user data to the database
-	query := "INSERT INTO users (id, password, height, age, allergies, diet, privacy, goal) 
-			VALUES (" + user.ID + ", " + user.Password + ", " + user.Height + ", " + user.age 
-			+ ", " + user.allergies + ", " + user.diet + ", " + user.privacy + ", " + user.goal + ")"
-	_, err := db.Exec(query)
+	query := "INSERT INTO users (id, password, height, age, allergies, diet, privacy, goal) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+	_, err := db.Exec(query, user.ID, user.Password, user.Height, user.age, user.allergies, user.diet, user.privacy, user.goal)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func readUserData(id int, db *sql.DB) UserData {
+func readUserData(id int, db *sql.DB) {
 	// read the user data from the database
-	query := "SELECT * FROM users WHERE id = " + id
+	query := "SELECT * FROM users WHERE id = " + strconv.Itoa(id)
 	rows, err := db.Query(query)
 	if err != nil {
 		panic(err)
 	}
-	Println(rows)
+	fmt.Println(rows)
+
 }
 
 func testDatabase() {
 	user := UserData{
-		ID: 1,
-		Password: "password",
-		Height: 5,
-		age: 20,
+		ID:        1,
+		Password:  "password",
+		Height:    5,
+		age:       20,
 		allergies: "none",
-		diet: "none",
-		privacy: true,
-		goal: "lose weight",
+		diet:      "none",
+		privacy:   true,
+		goal:      "lose weight",
 	}
-	sql = ConnectToDB()
-	writeUserData(user, sql)
-	readUserData(1, sql)
+	server.data = ConnectToDB()
+	writeUserData(user, server.data)
+	readUserData(1, server.data)
 }
-
